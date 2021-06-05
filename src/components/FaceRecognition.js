07e45facc5ff6927bf4658154d6124989030d184
rafal <jarorafo@gmail.com>
2021-06-05 22:22:47 +0200

    set default placeholder on image error

--- src/components/FaceRecognition.js
+++ src/components/FaceRecognition.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import face from './Logo/face.png'
 
 const FaceRecognition = ({ imageUrl, box }) => {
     return ( 
         <div className="face-box">
             <h2>Result</h2>
-            <div className="box-wrapper">
-                <img id = "inputImage" src={ imageUrl } alt="result of the app" />
-                <div className="bounding-box" style={{
-                    top: box.topRow,
-                    left: box.leftCol,
-                    bottom: box.bottomRow,
-                    right: box.rightCol
-                }}></div>
-            </div>
+            {
+                <div className="box-wrapper">
+                    <img id = "inputImage" src={ imageUrl } onError={(e) => (e.target.src= face) } alt="result of the app" />
+                    <div className="bounding-box" 
+                    style={{
+                        top: box.topRow,
+                        left: box.leftCol,
+                        bottom: box.bottomRow,
+                        right: box.rightCol
+                    }}>
+                    </div>
+                </div>
+            }
         </div>
      );
 }
